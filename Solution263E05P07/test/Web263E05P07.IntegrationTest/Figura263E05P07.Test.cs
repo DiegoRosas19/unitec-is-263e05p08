31c6207c11b549bca3eff5a6e3e61b3a918dce97
Alumno49 SUR <Alumno49@myunitecedumx>
2026-08-03 19:21:27 -0600

    Init

---
+++ Solution263E05P07/test/Web263E05P07.IntegrationTest/Figura263E05P07.Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+public class IntegrationTestPages : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+    public IntegrationTestPages(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+    [Theory]
+    [InlineData("")] 
+    [InlineData("Privacy")]
+    [InlineData("Privacy1")] 
+    [InlineData("About")]
+    [InlineData("About1")] 
+    public async Task Get_PaginasDeLaApp_RetornanStatusCode200YHtml(string url)
+    {
+
+        var client = _factory.CreateClient();
+
+        var response = await client.GetAsync(url);
+
+        response.EnsureSuccessStatusCode(); 
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode); 
+
+        Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+    }
+}
